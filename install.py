02ccef7d187843672f51007ecb7713f53539aa7e
Bruce Cota <bcota@unicon.net>
2018-08-13 15:14:36 -0400

    added comments to the install script

--- install.py
+++ install.py
@@ -8,7 +8,7 @@ import zipfile
 
 bucketName = os.environ['BUCKET_NAME']
 region = os.environ['REGION']
-rolePrefix = os.environ['ROLE_PREFIX']
+rolePrefix = os.environ['ROLE_PREFIX'] #should probably rename this since it's a unversal prefix
 username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
 
@@ -18,11 +18,41 @@ bucketArnWildCard = bucketArn + '/*'
 s3 = boto3.client('s3')
 iam = boto3.client('iam')
 
+#Policy that allows lambdas to assume the role
+lambdaTrustPolicy = {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+  }]
+}
 
+#Policy that allows to log themselves
+lambdaExecutionPolicy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        }
+    ]
+}
+
+
+#Create a role for lambdas
 def createRole(roleName, *policies):
     roleResponse = iam.create_role(
         RoleName=roleName,
-        AssumeRolePolicyDocument=json.dumps(lambdaTrustPolicy)
+        AssumeRolePolicyDocument=json.dumps(lambdaTrustPolicy) 
     )
     roleArn = roleResponse['Role']['Arn']
     p=0
@@ -78,6 +108,8 @@ else:
 bucketWaiter = s3.get_waiter('bucket_exists')
 bucketWaiter.wait(Bucket=bucketName)
 
+#Upload a zip file for creating a lambda and do it now since it might
+#take a few seconds to become visible across all AZs.
 thumbLambdaZipFile = 'thumbLambda.zip'
 copyToS3(thumbLambdaZipFile) 
     
@@ -87,34 +119,8 @@ s3.put_bucket_website(
         'IndexDocument': {'Suffix': 'index.html'}
     })
 
-lambdaTrustPolicy = {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-  }]
-}
 
-lambdaExecutionPolicy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "arn:aws:logs:*:*:*"
-        }
-    ]
-}
-
-
+#Policy that allows management of photos
 photoManagerPolicy = {
     "Version": "2012-10-17",
     "Statement": [
@@ -129,6 +135,7 @@ photoManagerPolicy = {
 photoManagerRoleName = rolePrefix + 'PhotoManager'
 photoManagerArn = createRole(photoManagerRoleName, photoManagerPolicy)
 
+#Allow the authorization to create a session for photo management
 authLambdaPolicy = {
     "Version": "2012-10-17",
     "Statement": [
@@ -163,7 +170,11 @@ bucketPolicy = {
     ]
 }
 
+#The put_bucket_policy often fails without some sleep.  Occasionally, it
+#fails even after 6 second sleep, but 6 seconds works for me most of
+#the time
 time.sleep(6)
+
 s3.put_bucket_policy(Bucket=bucketName, Policy=json.dumps(bucketPolicy))
 s3.put_bucket_cors(
     Bucket=bucketName,
@@ -183,7 +194,13 @@ lambdaClient = boto3.client('lambda')
 authLambdaCode = readFile('auth/auth.zip', 'rb')
 
 authLambdaName=rolePrefix + 'AuthLambda'
+
+#The create function occasionally fails without a sleep after creating the role.
 time.sleep(4)
+
+#Create the lambda that will authorize users for managing the photos.
+#The code for this is small so we include the body as a parameter to
+#the create_function call.
 response = lambdaClient.create_function(
     FunctionName = authLambdaName,
     Runtime = 'python3.6',
@@ -200,29 +217,31 @@ response = lambdaClient.create_function(
 )
 lambdaArn = response['FunctionArn']
 
+#Create the api gateway endpoint (from a swagger definition) for the auth lambda
 apig = boto3.client('apigateway')
 apiName = rolePrefix + 'AuthApi'
 apiResponse = apig.create_rest_api(name=apiName)
 apiId = apiResponse['id']
-
 swagger = readFile('photoauth-Production-swagger-apigateway.json', 'r').replace(
     '${apiId}', apiId).replace('${region}', region).replace('${authLambdaArn}', lambdaArn).replace('${title}', apiName)
 apig.put_rest_api(restApiId=apiId, body=swagger)
 apig.create_deployment(restApiId=apiId, stageName='Production')
 
+#Upload required javasccript and html files.  
 copyToS3('photoalbum.js')
 copyToS3('photos.js')
 replAndCopyToS3('index.html')
 replAndCopyToS3('index-template.html')
 replAndCopyToS3('private-index.html')
 
+#Create a javascript library for using the authorization endpoint.
+#This results in a downloaded zip file.  We unzip it and upload each
+#individual js file to the s3 bucket.
 zipFileName = 'sdk.zip'
-
 sdk = apig.get_sdk(restApiId=apiId, stageName='Production', sdkType='javascript')['body'].read()
 zipFile = open(zipFileName, 'wb')
 zipFile.write(sdk)
 zipFile.close()
-
 zip = zipfile.ZipFile(zipFileName, 'r')
 zip.extractall(".")
 zip.close()
@@ -236,8 +255,10 @@ def copyDir(dirName):
             copyToS3(fullpath)
 
 copyDir('apiGateway-js-sdk')
-thumbLambdaName = rolePrefix + 'ThumbLambda'
 
+#Create the lambda for processing images.  This is from a zip file,
+#which includes some image processing libraries.
+thumbLambdaName = rolePrefix + 'ThumbLambda'
 objectWaiter  = s3.get_waiter('object_exists')
 objectWaiter.wait(Bucket=bucketName, Key=thumbLambdaZipFile)
 thumbResponse = lambdaClient.create_function(
@@ -249,8 +270,10 @@ thumbResponse = lambdaClient.create_function(
 )
 thumbArn = thumbResponse['FunctionArn']
 
-uniqueId = str(datetime.datetime.utcnow().timestamp()).replace('.', '')
+#Create a rule to trigger the image processing lambda when an image is added to the bucket.
 
+#The add_permission insists on some kind of unique id that we never use later
+uniqueId = str(datetime.datetime.utcnow().timestamp()).replace('.', '')
 lambdaClient.add_permission(
     StatementId = uniqueId,
     FunctionName = thumbLambdaName,
